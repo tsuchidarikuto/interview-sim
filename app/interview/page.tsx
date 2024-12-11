4c07222a9800396a66b0ca5491447310d4d7a065
tsuchidarikuto <tsuchidarikuto@gmail.com>
2024-12-11 15:13:41 +0900

    面接機能の改善とヘッダーリンクの追加

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -13,11 +13,20 @@ export default function Interview() {
     
     const [conversation,setConversation] =useState<conversationTypes[]>([])
     const [isSend,setIsSend]=useState<boolean>(false)
+    const [questionIndex,setQuestionIndex]=useState<number>(0)
+    const [isEnd,setIsEnd]=useState<boolean>(false)
     async function handleSubmit(message:string){
         setIsSend(true)
         
-        setConversation([...conversation,{role:'user',message:message}]);
-        
+        setConversation((prev)=>[...prev,{role:'user',message:message}]);        
+
+        if(questions && questionIndex +1 < questions.length){
+            setConversation((prev)=>[...prev,{role:'system',message:questions[questionIndex+1]?.question}]);
+            setQuestionIndex((prev)=>prev+1);
+        }else if(questionIndex===questions.length){
+            setConversation((prev)=>[...prev,{role:'system',message:'面接終了です。'}]);
+            setIsEnd(true)
+        }
         setIsSend(false)        
 
     }
@@ -54,12 +63,13 @@ export default function Interview() {
                    
             </ChatContainer>
         </MainContainer>    
-            
+         {isEnd &&       
         <Link href='/result'>
             <Button variant="contained" size='small'>
                 結果へ
             </Button>
         </Link>
+        }
         </Container>
     );
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 
-interface HeaderProps{
-    title: string;
-}
-export default function Header({title}: HeaderProps) {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" >{title}</Typography>
-            </Toolbar>
-        </AppBar>
-    )
+interface HeaderProps {
+  title: string;
 }
 
+export default function Header({ title }: HeaderProps) {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Link href="/" passHref>
+          <Typography variant="h6" component="a">
+            {title}
+          </Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+}
