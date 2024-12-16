8c22288d131a3c45bc529cf41806bc13f8313a95
tsuchidarikuto <tsuchidarikuto@gmail.com>
2024-12-16 12:42:31 +0900

    結果ページの合否メッセージを改善し、採用・不採用の表示に色を追加。カードコンポーネントのスタイルを調整。

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -12,13 +12,19 @@ export default function Result() {
     return (
         <Container maxWidth="md" sx={{ mt: 5, mb: 4, height: '80vh' }}>
             <Typography variant="h4" component="h1">結果</Typography>
-            <Divider sx={{ width: '100%', bgcolor: 'primary.main' }} />
+            <Divider sx={{ width: '100%', bgcolor: 'primary.main', mb: 2}} />
             
                 <Stack spacing={5}>
+                    <Card sx={{ p: 2 }} variant="outlined">
                     {interviewResult.isPass ?
-                        <Typography variant="h5">厳正な審査の結果、採用させていただきます。</Typography>
-                        : <Typography variant="h5">残念ながら不採用となりました。</Typography>
+                        <Typography variant="h5">
+                            厳正な審査の結果、<span style={{ color: 'green' }}>採用</span>が決定しました。
+                        </Typography>
+                        : <Typography variant="h5">
+                            残念ながら今回は<span style={{ color: 'red' }}>不採用</span>となりました。
+                        </Typography>
                     }
+                    </Card>
                     <Grid container spacing={2} alignItems="center" justifyContent="center">
                         <Grid size={{ xs: 12, md: 12 }}>
                             <Box sx={{ width: "80%", margin: "0 auto" }}>
@@ -27,13 +33,13 @@ export default function Result() {
                         </Grid>
                         <Grid container spacing={2}>
                             <Grid size={{ xs: 12, md: 6 }}>
-                                <Card sx={{ height: '100%', width: '100%' }}>
+                                <Card sx={{ height: '100%', width: '100%' }}variant="outlined">
                                     <Typography variant="h6" component="h2">良かった点</Typography>
                                     <Typography variant="body1" component="p">{interviewResult.feedback.positive}</Typography>
                                 </Card>
                             </Grid>
                             <Grid size={{ xs: 12, md: 6 }}>
-                                <Card sx={{ height: '100%', width: '100%' }}>
+                                <Card sx={{ height: '100%', width: '100%' }}variant="outlined">
                                     <Typography variant="h6" component="h2">改善点</Typography>
                                     <Typography variant="body1" component="p">{interviewResult.feedback.negative}</Typography>
                                 </Card>
