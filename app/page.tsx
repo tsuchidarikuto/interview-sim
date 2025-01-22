b3584d8a4a52dfc624c83208a4350f8ef19d6b0a
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-01-22 14:48:51 +0900

    面接開始ボタンを追加し、インタビュー準備中にプログレスバーを表示する機能を実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,58 @@
 'use client';
-import {useState} from 'react';
-import {Box, Tab, Typography,Container} from '@mui/material';
+import {useState,useContext} from 'react';
+import {Box, Tab, Typography,Container,Button} from '@mui/material';
 import Company from '@/components/Company';
 import Resume from '@/components/Resume';
 import {TabList,TabContext,TabPanel} from '@mui/lab';
 import InterviewSetting from '@/components/InterviewSetting';
+import LinearProgressWithLabel from '@/components/LinearProgressWithLabel';
+import { useRouter}  from "next/navigation"
+import {useAtom} from "jotai"
+import {questionsAtom,companyAtom,settingAtom,resumeAtom} from "@/atoms/state";
+import {AuthContext} from "@/provider/AuthContext"
+import { PreparationInterview } from '@/utils/PreparationInterview';
+
+
 export default function CenteredTabs() {
+  const { user } = useContext(AuthContext);
+  const { push } = useRouter();
   const [value, setValue] = useState("1");
+  const [isLoadInterview,setIsLoadInterview] = useState<boolean>(false);
+  const [,setQuestions]=useAtom(questionsAtom);
+  const [,setResume] = useAtom (resumeAtom);
+  const [,setCompany] = useAtom (companyAtom);
+  const [, setSetting] = useAtom(settingAtom);
+  const [progress, setProgress] = useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const handleStartInterview = async () => {
+          setIsLoadInterview(true);
+          try {
+              if (!user) {
+                  throw new Error('User not found');
+              }            
+              await PreparationInterview(setProgress,setQuestions,setResume,setCompany,setSetting, user.uid);
+  
+              setProgress(100);
+              push('/interview');
+              
+          } catch (e) {
+              console.error('Error during preparation:', e);
+          }
+          
+      };
+  if(isLoadInterview){
+    return(
+      <Box sx={{display:"flex",alignItems:"center",justifyContent:"center",height:"60vh" }}>
+        <Box sx={{width:"90%" }}>
+          <LinearProgressWithLabel value={progress} />
+        </Box>
+      </Box>
+    )
+  }
 
   return (
 
@@ -26,7 +68,7 @@ export default function CenteredTabs() {
             </Typography>
         
       
-  
+      <Box sx = {{ height:"70vh"}}>
       <TabContext value={value} >
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example" centered >
@@ -39,6 +81,20 @@ export default function CenteredTabs() {
         <TabPanel value="2"><Company/></TabPanel>
         <TabPanel value="3"><InterviewSetting/></TabPanel>
       </TabContext>
+      </Box>
+      
+      
+                        <Box sx ={{display:"flex",justifyContent:"center"}}>
+                            <Button
+                                size="large"
+                                variant="contained"
+                                sx={{ width: '90%' }}
+                                onClick={handleStartInterview}
+                            >
+                                面接開始
+                            </Button>
+                            </Box>
+              
       </Container>
   
   );
