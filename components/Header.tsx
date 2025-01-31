50e6a4d1119434b30123c67078ffdede848a0492
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-01-31 13:38:44 +0900

    ヘッダーコンポーネントのコード整形とFirebaseからの履歴取得ロジックの改善

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,15 @@
 'use client';
-import React, { useContext, useEffect,useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography, Button, Box ,Badge} from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Badge } from '@mui/material';
 import { signOut } from 'firebase/auth';
-
 import { AuthContext } from '@/provider/AuthContext';
-import { auth } from '@/firebase';
-
+import { auth, firestore } from '@/firebase';
 import { HistoryTypes } from '@/types';
-
 import EmailIcon from '@mui/icons-material/Email';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import { collection, onSnapshot, query, where } from 'firebase/firestore';
-import { firestore } from '@/firebase';
-
 
 interface HeaderProps {
   title: string;
@@ -22,100 +17,115 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps) {
   const { user } = useContext(AuthContext);
-  const isLogin = !!user; 
-  const [history, setHistory] = useState<HistoryTypes[]>([])
-
-  const [unreadedCount,setUnreadedCount] = useState<number>(0)
+  const isLogin = !!user;
+  const [history, setHistory] = useState<HistoryTypes[]>([]);
+  const [unreadedCount, setUnreadedCount] = useState<number>(0);
 
   useEffect(() => {
     if (!user) {
       setUnreadedCount(0);
-        setHistory([]); // ユーザーがログアウトしたら履歴をリセット
+      setHistory([]);
       return;
     }
 
-    const q = query(
-        collection(firestore, 'history'),
-        where('uid', '==', user.uid)
-      );
-
+    const q = query(collection(firestore, 'history'), where('uid', '==', user.uid));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      const newHistory:HistoryTypes[] = [];
-      snapshot.forEach((doc) => {
-        newHistory.push( {
-          id:doc.id,
-          ...(doc.data() as Omit<HistoryTypes, 'id'>),
-        } );
-      });
+      const newHistory = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...(doc.data() as Omit<HistoryTypes, 'id'>),
+      }));
       setHistory(newHistory);
     }, (error) => {
-        console.error("Error fetching history: ", error);
-      });
-      return () => unsubscribe();
+      console.error('Error fetching history: ', error);
+    });
+
+    return () => unsubscribe();
   }, [user]);
 
-    useEffect(() => {
-        const countUnreaded = history.filter(item => !item.isRead).length;
-        setUnreadedCount(countUnreaded);
-      }, [history]);
+  useEffect(() => {
+    setUnreadedCount(history.filter((item) => !item.isRead).length);
+  }, [history]);
 
   return (
     <AppBar position="sticky">
-      <Toolbar >
+      <Toolbar>
         {isLogin ? (
-          
-            <Link href="/" passHref>            
-            <Box 
-              sx={{ 
-              backgroundImage: 'url(/titleLogo.svg)', 
-              backgroundSize: 'contain', 
-              backgroundRepeat: 'no-repeat', 
-              backgroundPosition: 'center', 
-              height: { xs: 28, sm: 50 }, 
-              width: { xs: 100, sm: 150 }, 
-              marginRight: 'auto' 
-              }} 
-            />          
-            </Link>):
-        (
+          <Link href="/" passHref>
+            <Box
+              sx={{
+                backgroundImage: 'url(/titleLogo.svg)',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                height: { xs: 28, sm: 50 },
+                width: { xs: 100, sm: 150 },
+                marginRight: 'auto',
+              }}
+            />
+          </Link>
+        ) : (
           <Typography variant="h6">
-            <strong>{title} </strong>
+            <strong>{title}</strong>
           </Typography>
         )}
 
         <div style={{ flexGrow: 1 }} />
         {isLogin && (
           <>
-            <Typography variant='body2' sx={{ mt: { xs: 1, sm: 0 }, fontSize: { xs: '0.75rem', sm: '1rem' } }}>
+            <Typography
+              variant="body2"
+              sx={{ mt: { xs: 1, sm: 0 }, fontSize: { xs: '0.75rem', sm: '1rem' } }}
+            >
               {user.email}
             </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', mx: { xs: 0, sm: 1 } }}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                mx: { xs: 0, sm: 1 },
+              }}
+            >
               <Button onClick={() => signOut(auth)}>
-          <LogoutOutlinedIcon sx={{ cursor: 'pointer', fontSize: { xs: '1.25rem', sm: '1.5rem' } }} color="secondary" />
+                <LogoutOutlinedIcon
+                  sx={{ cursor: 'pointer', fontSize: { xs: '1.25rem', sm: '1.5rem' } }}
+                  color="secondary"
+                />
               </Button>
             </Box>
-          </>
-        )}
-        {isLogin && (
-          <>
             <Link href="/mailbox" passHref>
-              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', mx: { xs: 0, sm: 1 } }}>
-          <Badge color="error" badgeContent={unreadedCount}>
-            <EmailIcon sx={{ cursor: 'pointer', fontSize: { xs: '1.25rem', sm: '1.5rem' } }} />
-          </Badge>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                  mx: { xs: 0, sm: 1 },
+                }}
+              >
+                <Badge color="error" badgeContent={unreadedCount}>
+                  <EmailIcon sx={{ cursor: 'pointer', fontSize: { xs: '1.25rem', sm: '1.5rem' } }} />
+                </Badge>
+              </Box>
+            </Link>
+            <Link href="/ranking" passHref>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                  mx: { xs: 0, sm: 1 },
+                }}
+              >
+                <EmojiEventsIcon
+                  sx={{ cursor: 'pointer', fontSize: { xs: '1.25rem', sm: '1.5rem' } }}
+                />
               </Box>
             </Link>
           </>
         )}
-        
-        <Link href="/ranking" passHref>
-              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', mx: { xs: 0, sm: 1 } }}>
-          
-            <EmailIcon sx={{ cursor: 'pointer', fontSize: { xs: '1.25rem', sm: '1.5rem' } }} />
-          
-              </Box>
-            </Link>
-        
       </Toolbar>
     </AppBar>
   );
