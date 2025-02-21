8e91e48913fb99c766c953246bc252abb22eb78a
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-02-21 23:56:52 +0900

    refactor: Supabaseクライアント作成関数のコメントを削除

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,16 +1,6 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-/**
- * Supabaseクライアントを作成する非同期関数。
- * 
- * この関数は、クッキーストアを取得し、Supabaseクライアントを作成して返します。
- * クッキーの取得と設定のためのカスタムロジックを含んでいます。
- * 
- * @returns {Promise<any>} Supabaseクライアントを含むPromise。
- * 
- * @throws 環境変数 `NEXT_PUBLIC_SUPABASE_URL` または `NEXT_PUBLIC_SUPABASE_ANON_KEY` が設定されていない場合。
- */
 export async function createClient() {
   const cookieStore = await cookies()
 
