8dcf641924ec35a30cdf338c67f5546dd8500175
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-03-03 09:13:07 +0900

    refactor:サンプルデータを別ファイルに保存

---
+++ app/auth/SampleData.ts
@@ -0,0 +1,29 @@
+import { ResumeTypes } from "@/types";
+
+export const sampleResumeData: ResumeTypes = {
+    uid: "",
+    id: "",
+    name: "サンプル 太郎",
+    birthday: "2000-01-01",
+    age: "22",
+    sex: 0,
+    education: "サンプル大学",
+    programming: "JavaScript, TypeScript",
+    selfProduction: "情熱的なエンジニアです。",
+    research: "最新技術の習得",
+    qualification: "各種資格",
+    studentAchievements: "学業・部活両立",
+    reasonForApply: "チャレンジ精神"
+};
+
+export const sampleCompanyData = {
+    uid: "",
+    id: "",
+    name: "サンプル株式会社",
+    position: "エンジニア",
+    skillset: "フロントエンド, バックエンド",
+    mission: "世の中を変える",
+    product: "革新的なサービス",
+    culture: "オープンで自由",
+    others: "未定"
+}
