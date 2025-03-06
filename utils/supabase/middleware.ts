a70c747ea62670cfb27a7b5126828b2a3c17f364
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-03-06 12:59:32 +0900

    refactor: 認証関連のパスをミドルウェアでのリダイレクト条件に追加

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,7 +40,9 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/terms') &&
+    !request.nextUrl.pathname.startsWith('/privacy')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
